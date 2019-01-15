45856d06fc60789bd1435fbd2f0cfd02e520ec91
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-01-15 20:14:15 +0900

    タップすると特定の四角形が赤・青・黄に変化するボタンを設置した

--- app/src/main/java/com/ntetz/android/colormyview/MainActivity.kt
+++ app/src/main/java/com/ntetz/android/colormyview/MainActivity.kt
@@ -22,7 +22,10 @@ class MainActivity : AppCompatActivity() {
                         box_three_text,
                         box_four_text,
                         box_five_text,
-                        constraint_layout
+                        constraint_layout,
+                        red_button,
+                        green_button,
+                        yellow_button
                 )
         for (item in clickableViews) {
             item.setOnClickListener { this.makeColored(it) }
@@ -41,6 +44,10 @@ class MainActivity : AppCompatActivity() {
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
 
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
