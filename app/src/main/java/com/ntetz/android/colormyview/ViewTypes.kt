9b810881b3aec22ee2db57c18647523d12d1437f
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-01-16 11:40:10 +0900

    MainActivity外にenumの定義を作った

---
+++ app/src/main/java/com/ntetz/android/colormyview/ViewTypes.kt
@@ -0,0 +1,13 @@
+package com.ntetz.android.colormyview
+
+enum class ViewTypes {
+    BOX_ONE,
+    BOX_TWO,
+    BOX_THREE,
+    BOX_FOUR,
+    BOX_FIVE,
+    BUTTON_RED,
+    BUTTON_GREEN,
+    BUTTON_YELLOW,
+    BACKGROUND;
+}
